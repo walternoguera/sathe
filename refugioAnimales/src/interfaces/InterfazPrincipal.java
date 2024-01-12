6cc07d749806bcbb1b82e0e9f2ab5a7463293721
walternoguera <walter.morel.noguera@gmail.com>
2024-01-12 10:42:29 +0100

    Añadiendo el paquete 'interfaces' con la clase 'InterfazPrincipal'

---
+++ refugioAnimales/src/interfaces/InterfazPrincipal.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import javax.swing.JFrame;
+
+public class InterfazPrincipal extends JFrame {
+
+    public InterfazPrincipal() {
+        setTitle("Sathe");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        
+    }
+}
