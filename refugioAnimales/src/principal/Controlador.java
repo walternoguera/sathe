d86de2906fefac4a0ecfa078ea165ac31d643a28
walternoguera <walter.morel.noguera@gmail.com>
2024-01-12 10:43:25 +0100

    Añadiendo el paquete 'principal' que contiene la clase 'Controlador'

---
+++ refugioAnimales/src/principal/Controlador.java
@@ -0,0 +1,11 @@
+package principal;
+
+import interfaces.InterfazPrincipal;
+
+public class Controlador {
+
+    public static void main(String[] args) {
+        InterfazPrincipal interfaz = new InterfazPrincipal();
+        interfaz.setVisible(true);
+    }
+}
