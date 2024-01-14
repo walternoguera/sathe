83ef8a24845011055e2d8101c0e157fece46dd0d
walternoguera <walter.morel.noguera@gmail.com>
2024-01-14 23:41:24 +0100

    refactorizando código

--- refugioAnimales/src/views/Voluntarios.java
+++ refugioAnimales/src/views/Voluntarios.java
@@ -181,22 +181,28 @@ public class Voluntarios extends javax.swing.JPanel {
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Datos"));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Datos", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(102, 102, 102))); // NOI18N
 
+        jLabel1.setForeground(new java.awt.Color(102, 102, 102));
         jLabel1.setText("ID:");
 
         txtId.setEditable(false);
         txtId.setFont(new java.awt.Font("Segoe UI Black", 1, 12)); // NOI18N
         txtId.setEnabled(false);
 
+        nombre.setForeground(new java.awt.Color(102, 102, 102));
         nombre.setText("NOMBRE:");
 
+        telefono.setForeground(new java.awt.Color(102, 102, 102));
         telefono.setText("TELEFONO");
 
+        localidad.setForeground(new java.awt.Color(102, 102, 102));
         localidad.setText("LOCALIDAD");
 
+        habilidades.setForeground(new java.awt.Color(102, 102, 102));
         habilidades.setText("HABILIDADES");
 
+        apellidos.setForeground(new java.awt.Color(102, 102, 102));
         apellidos.setText("APELLIDOS");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -273,7 +279,7 @@ public class Voluntarios extends javax.swing.JPanel {
         add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 740, 220));
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Registros Actuales:"));
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Registros Actuales:", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(102, 102, 102))); // NOI18N
 
         Tabla.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -305,9 +311,11 @@ public class Voluntarios extends javax.swing.JPanel {
 
         add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 240, 740, 200));
 
-        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Acciones"));
+        jPanel3.setBackground(new java.awt.Color(33, 36, 46));
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Acciones", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(186, 187, 190))); // NOI18N
 
+        btnModificar.setBackground(new java.awt.Color(33, 36, 46));
+        btnModificar.setForeground(new java.awt.Color(186, 187, 190));
         btnModificar.setText("Modificar");
         btnModificar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnModificar.addActionListener(new java.awt.event.ActionListener() {
@@ -316,6 +324,8 @@ public class Voluntarios extends javax.swing.JPanel {
             }
         });
 
+        btnAgregar.setBackground(new java.awt.Color(33, 36, 46));
+        btnAgregar.setForeground(new java.awt.Color(186, 187, 190));
         btnAgregar.setText("Añadir");
         btnAgregar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnAgregar.addActionListener(new java.awt.event.ActionListener() {
@@ -324,6 +334,8 @@ public class Voluntarios extends javax.swing.JPanel {
             }
         });
 
+        btnEliminar.setBackground(new java.awt.Color(33, 36, 46));
+        btnEliminar.setForeground(new java.awt.Color(186, 187, 190));
         btnEliminar.setText("Borrar");
         btnEliminar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnEliminar.addActionListener(new java.awt.event.ActionListener() {
@@ -332,6 +344,8 @@ public class Voluntarios extends javax.swing.JPanel {
             }
         });
 
+        btnNuevo.setBackground(new java.awt.Color(33, 36, 46));
+        btnNuevo.setForeground(new java.awt.Color(186, 187, 190));
         btnNuevo.setText("Nuevo");
         btnNuevo.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnNuevo.addActionListener(new java.awt.event.ActionListener() {
