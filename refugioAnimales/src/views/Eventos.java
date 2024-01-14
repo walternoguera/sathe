2f165dfa54089f3c03c9d0c7d1a85e2f46a08b83
walternoguera <walter.morel.noguera@gmail.com>
2024-01-14 02:36:20 +0100

    refactorizando código de la vista Eventos

--- refugioAnimales/src/views/Eventos.java
+++ refugioAnimales/src/views/Eventos.java
@@ -1,42 +1,391 @@
 package views;
 
+import database.Conexion;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+import javax.swing.table.DefaultTableModel;
+
 public class Eventos extends javax.swing.JPanel {
 
+    // Conexión y variables relacionadas con la base de datos
+    Connection conet;
+    Statement st;
+    ResultSet rs;
+    int idc;
+
+    // Modelo para la tabla
+    DefaultTableModel modelo;
+
+    // Constructor de la clase
     public Eventos() {
         initComponents();
+        consultar();
+    }
+
+    //metodos CRUD
+    private void consultar() {
+        String query = "SELECT * FROM eventos";
+
+        try {
+            conet = Conexion.getConnection();
+            st = conet.createStatement();
+            rs = st.executeQuery(query);
+            modelo = (DefaultTableModel) Tabla.getModel();
+            modelo.setRowCount(0);
+            Object[] mascota = new Object[4]; //numero de columna de la tabla
+            while (rs.next()) {
+                mascota[0] = rs.getInt("ID");
+                mascota[1] = rs.getString("NOMBRE_EVENTO");
+                mascota[2] = rs.getString("FECHA");
+                mascota[3] = rs.getString("DESCRIPCION");
+
+                modelo.addRow(mascota.clone());
+            }
+            Tabla.setModel(modelo);
+
+        } catch (Exception e) {
+        }
+
+    }
+
+    private void agregar() {
+        String nombreEventoCadena = txtNombreEvento.getText();
+        String fechaCadena = txtFecha.getText();
+        String descripcionEventoCadena = txtDescripcionEvento.getText();
+
+        try {
+            if (nombreEventoCadena.equals("") || fechaCadena.equals("") || descripcionEventoCadena.equals("")) {
+                JOptionPane.showMessageDialog(null, "Por favor, añada los datos");
+                resetearFormulario();
+            } else {
+
+                String query = "INSERT into eventos(NOMBRE_EVENTO, FECHA, DESCRIPCION) values ('" + nombreEventoCadena + "', '" + fechaCadena + "', '" + descripcionEventoCadena + "')";
+                conet = Conexion.getConnection();
+                st = conet.createStatement();
+                st.executeUpdate(query);
+                JOptionPane.showMessageDialog(null, "Nuevo registro agregado");
+                consultar();
+                resetearFormulario();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void modificar() {
+        String nombreEventoCadena = txtNombreEvento.getText();
+        String fechaCadena = txtFecha.getText();
+        String descripcionEventoCadena = txtDescripcionEvento.getText();
+
+        try {
+            if (nombreEventoCadena.equals("") || fechaCadena.equals("") || descripcionEventoCadena.equals("")) {
+                JOptionPane.showMessageDialog(null, "Por favor, seleccione el registro que deseas modificar");
+                resetearFormulario();
+            } else {
+
+                String query = "UPDATE eventos SET nombre_evento='" + nombreEventoCadena + "', fecha='" + fechaCadena + "', descripcion='" + descripcionEventoCadena + "' WHERE ID=" + idc;
+                conet = Conexion.getConnection();
+                st = conet.createStatement();
+                st.executeUpdate(query);
+                JOptionPane.showMessageDialog(null, "Registro modificado");
+                consultar();
+                resetearFormulario();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void eliminar() {
+        int fila = Tabla.getSelectedRow();
+        try {
+            if (fila < 0) {
+                JOptionPane.showMessageDialog(null, "Por favor, selecciona un registro");
+                resetearFormulario();
+            } else {
+                String query = "delete from eventos where id=" + idc;
+                conet = Conexion.getConnection();
+                st = conet.createStatement();
+                st.executeUpdate(query);
+                JOptionPane.showMessageDialog(null, "Registro eliminado");
+                resetearFormulario();
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void nuevo() {
+
+        txtId.setText("");
+        txtNombreEvento.setText("");
+        txtFecha.setText("");
+        txtDescripcionEvento.setText("");
+        txtId.requestFocus();
+
+    }
+
+    private void resetearFormulario() {
+        txtId.setText("");
+        txtNombreEvento.setText("");
+        txtFecha.setText("");
+        txtDescripcionEvento.setText("");
+
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        txtId = new javax.swing.JTextField();
+        tituloEvento = new javax.swing.JLabel();
+        txtNombreEvento = new javax.swing.JTextField();
+        fecha = new javax.swing.JLabel();
+        txtFecha = new javax.swing.JTextField();
+        descripcionEvento = new javax.swing.JLabel();
+        txtDescripcionEvento = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        Tabla = new javax.swing.JTable();
+        jPanel3 = new javax.swing.JPanel();
+        btnModificar = new javax.swing.JButton();
+        btnAgregar = new javax.swing.JButton();
+        btnEliminar = new javax.swing.JButton();
+        btnNuevo = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(255, 255, 255));
+        setToolTipText("datos");
         setPreferredSize(new java.awt.Dimension(790, 510));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setText("eventos");
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Datos"));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+        jLabel1.setText("ID:");
+
+        txtId.setEditable(false);
+        txtId.setFont(new java.awt.Font("Segoe UI Black", 1, 12)); // NOI18N
+        txtId.setEnabled(false);
+
+        tituloEvento.setText("TITULO DEL EVENTO:");
+
+        fecha.setText("FECHA:");
+
+        descripcionEvento.setText("DESCRIPCIÓN:");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(txtDescripcionEvento)
+                        .addContainerGap())
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(tituloEvento)
+                                .addGap(18, 18, 18)
+                                .addComponent(txtNombreEvento, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 184, Short.MAX_VALUE)
+                                .addComponent(fecha, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(txtFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(82, 82, 82))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(descripcionEvento)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(tituloEvento)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(txtNombreEvento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(txtFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(fecha)))
                 .addGap(22, 22, 22)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(506, Short.MAX_VALUE))
+                .addComponent(descripcionEvento)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(txtDescripcionEvento, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(15, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(29, 29, 29)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(454, Short.MAX_VALUE))
+
+        add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 740, 220));
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Registros Actuales:"));
+
+        Tabla.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ID", "NOMBRE_EVENTO", "FECHA", "DESCRIPCIÓN"
+            }
+        ));
+        Tabla.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                TablaMouseClicked(evt);
+            }
+        });
+        jScrollPane2.setViewportView(Tabla);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 730, Short.MAX_VALUE)
         );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE))
+        );
+
+        add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 240, 740, 200));
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Acciones"));
+
+        btnModificar.setText("Modificar");
+        btnModificar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnModificarActionPerformed(evt);
+            }
+        });
+
+        btnAgregar.setText("Añadir");
+        btnAgregar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarActionPerformed(evt);
+            }
+        });
+
+        btnEliminar.setText("Borrar");
+        btnEliminar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEliminarActionPerformed(evt);
+            }
+        });
+
+        btnNuevo.setText("Nuevo");
+        btnNuevo.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnNuevo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNuevoActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(145, 145, 145)
+                .addComponent(btnAgregar, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(12, 12, 12)
+                .addComponent(btnModificar, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22)
+                .addComponent(btnEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22)
+                .addComponent(btnNuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(2, 2, 2)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAgregar)
+                    .addComponent(btnModificar)
+                    .addComponent(btnEliminar)
+                    .addComponent(btnNuevo)))
+        );
+
+        add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 440, 740, 60));
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActionPerformed
+        agregar();
+        consultar();
+        nuevo();
+    }//GEN-LAST:event_btnAgregarActionPerformed
+
+    private void btnModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarActionPerformed
+        modificar();
+        consultar();
+        nuevo();
+    }//GEN-LAST:event_btnModificarActionPerformed
+
+    private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarActionPerformed
+        eliminar();
+        consultar();
+        nuevo();
+    }//GEN-LAST:event_btnEliminarActionPerformed
+
+    private void btnNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNuevoActionPerformed
+        nuevo();
+    }//GEN-LAST:event_btnNuevoActionPerformed
+
+    private void TablaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TablaMouseClicked
+        int fila = Tabla.getSelectedRow();
+        if (fila == -1) {
+            JOptionPane.showMessageDialog(null, "Por favor, seleccione una fila");
+        } else {
+            idc = Integer.parseInt((String) Tabla.getValueAt(fila, 0).toString());
+            String nombreEventoCadena = (String) Tabla.getValueAt(fila, 1).toString();
+            String fechaCadena = (String) Tabla.getValueAt(fila, 2).toString();
+            String descripcionEventoCadena = (String) Tabla.getValueAt(fila, 3).toString();
+
+            txtId.setText("" + idc);
+            txtNombreEvento.setText(nombreEventoCadena);
+            txtFecha.setText(fechaCadena);
+            txtDescripcionEvento.setText(descripcionEventoCadena);
+
+        }
+    }//GEN-LAST:event_TablaMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable Tabla;
+    private javax.swing.JButton btnAgregar;
+    private javax.swing.JButton btnEliminar;
+    private javax.swing.JButton btnModificar;
+    private javax.swing.JButton btnNuevo;
+    private javax.swing.JLabel descripcionEvento;
+    private javax.swing.JLabel fecha;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel tituloEvento;
+    private javax.swing.JTextField txtDescripcionEvento;
+    private javax.swing.JTextField txtFecha;
+    private javax.swing.JTextField txtId;
+    private javax.swing.JTextField txtNombreEvento;
     // End of variables declaration//GEN-END:variables
 }
